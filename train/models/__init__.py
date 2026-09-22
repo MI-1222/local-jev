a80dfe3021cad97fc2b92472860e2299bd8b3fce
MI-1222 <rohisamari1222@gmail.com>
2026-09-23 00:35:45 +0900

    feat(train): 日本語バックボーン(`sbintuitions/modernbert-ja-130m`)換装, [OP]トークン初期化の改善

--- train/models/__init__.py
+++ train/models/__init__.py
@@ -1,8 +1,11 @@
 """Local-Jev モデル層パッケージ。"""
 
 from .backbone import (
+    DEFAULT_BACKBONE_MODEL_ID,
     DEFAULT_MMBERT_MODEL_ID,
+    DEFAULT_MODERNBERT_JA_MODEL_ID,
     DEFAULT_MODERNBERT_MODEL_ID,
+    initialize_token_embedding_with_normalized_centroid,
     prepare_backbone_and_tokenizer,
     save_tokenizer_for_runtime,
     verify_option_marker_tokenization,
@@ -10,11 +13,14 @@ from .backbone import (
 from .decision_head import DecisionHead, JevDecisionModel, OptionGatherLayer
 
 __all__ = [
+    "DEFAULT_BACKBONE_MODEL_ID",
     "DEFAULT_MMBERT_MODEL_ID",
+    "DEFAULT_MODERNBERT_JA_MODEL_ID",
     "DEFAULT_MODERNBERT_MODEL_ID",
     "DecisionHead",
     "JevDecisionModel",
     "OptionGatherLayer",
+    "initialize_token_embedding_with_normalized_centroid",
     "prepare_backbone_and_tokenizer",
     "save_tokenizer_for_runtime",
     "verify_option_marker_tokenization",
